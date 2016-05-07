893683abc9e594a1a45e90f236940f23aa974c71
Paul Loyd <pavelko95@gmail.com>
2016-05-07 11:09:21 +0300

    feat: convert div to p if needed

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,13 @@ use std::collections::HashMap;
 use std::ops::Deref;
 use std::hash::{Hash, Hasher};
 
-use kuchiki::{Node, NodeRef};
+use string_cache::QualName;
+use kuchiki::{Node, NodeRef, NodeDataRef, ElementData};
 use kuchiki::traits::TendrilSink;
 use kuchiki::iter::{NodeEdge, NodeIterator};
 
+
+type ElemRef = NodeDataRef<ElementData>;
 
 #[derive(PartialEq, Eq)]
 struct HashableNodeRef(NodeRef);
@@ -29,6 +32,52 @@ impl Hash for HashableNodeRef {
     }
 }
 
+trait NodeRefExt {
+    fn node_ref(&self) -> &NodeRef;
+
+    fn is(&self, name: QualName) -> bool {
+        self.node_ref().as_element().map_or(false, |e| e.name == name)
+    }
+
+    fn replace<N: NodeRefExt>(&self, node: N) {
+        self.node_ref().insert_before(node.node_ref().clone());
+        self.remove();
+    }
+
+    fn remove(&self) {
+        self.node_ref().detach();
+    }
+
+    fn rename(&self, name: QualName) {
+        let node = self.node_ref();
+
+        if let Some(elem) = node.as_element() {
+            let mut attributes = elem.attributes.borrow_mut();
+            let replacement = NodeRef::new_element(name, attributes.map.drain());
+
+            for child in node.children() {
+                replacement.append(child);
+            }
+
+            node.replace(replacement);
+        }
+    }
+}
+
+impl NodeRefExt for NodeRef {
+    #[inline]
+    fn node_ref(&self) -> &NodeRef {
+        self
+    }
+}
+
+impl NodeRefExt for ElemRef {
+    #[inline]
+    fn node_ref(&self) -> &NodeRef {
+        self.as_node()
+    }
+}
+
 // Must be sorted.
 static UNLIKELY_CANDIDATE: [&'static str; 23] = [
     "ad-break", "agegate", "banner", "combx", "comment", "community", "disqus", "extra", "foot",
@@ -38,6 +87,10 @@ static UNLIKELY_CANDIDATE: [&'static str; 23] = [
 
 // Must be sorted.
 static MAYBE_CANDIDATE: [&'static str; 6] = ["and", "article", "body", "column", "main", "shadow"];
+
+macro_rules! tag {
+    ($name:tt) => { qualname!(html, $name) };
+}
 
 pub struct Readability {
     scores: HashMap<HashableNodeRef, u32>
@@ -59,54 +112,91 @@ impl Readability {
     fn readify(&mut self, top_level: NodeRef) {
         for edge in top_level.traverse() {
             match edge {
-                NodeEdge::Start(this) => {
-                    self.strip_unlikely_children(this);
+                NodeEdge::Start(node) => {
+                    for child in node.children().elements() {
+                        if self.is_unlikely_candidate(&child) {
+                            child.remove();
+                        } else if child.is(tag!("div")) {
+                            self.convert_div_if_needed(&child);
+                        }
+                    }
                 },
 
-                NodeEdge::End(this) => {
-                }
+                NodeEdge::End(node) => {}
             }
         }
     }
 
-    fn strip_unlikely_children(&self, node: NodeRef) {
-        for child in node.children().elements() {
-            match child.name {
-                qualname!(html, "a") | qualname!(html, "body") => continue,
-                _ => {}
+    fn is_unlikely_candidate(&self, elem: &ElemRef) -> bool {
+        match elem.name {
+            tag!("a") | tag!("body") => return false,
+            _ => {}
+        }
+
+        let attributes = elem.attributes.borrow();
+
+        // Ok, check classes.
+        let mut unlikely = false;
+
+        let classes = attributes.get(atom!("class")).unwrap_or("");
+        let id = attributes.get(atom!("id")).unwrap_or("");
+
+        let class_it = classes
+            .split_whitespace()
+            .map(|c| c.to_lowercase());
+
+        let id_it = id
+            .split_whitespace()
+            .map(|c| c.to_lowercase());
+
+        for word in class_it.chain(id_it) {
+            if MAYBE_CANDIDATE.binary_search(&&word[..]).is_ok() {
+                unlikely = false;
+                break;
             }
 
-            let attributes = child.attributes.borrow();
-
-            // Ok, check classes.
-            let mut unlikely = false;
-
-            let classes = attributes.get(atom!("class")).unwrap_or("");
-            let id = attributes.get(atom!("id")).unwrap_or("");
-
-            let class_it = classes
-                .split_whitespace()
-                .map(|c| c.to_lowercase());
-
-            let id_it = id
-                .split_whitespace()
-                .map(|c| c.to_lowercase());
-
-            for word in class_it.chain(id_it) {
-                if MAYBE_CANDIDATE.binary_search(&&word[..]).is_ok() {
-                    unlikely = false;
-                    break;
-                }
-
-                if !unlikely && UNLIKELY_CANDIDATE.binary_search(&&word[..]).is_ok() {
-                    unlikely = true;
-                }
-            }
-
-            if unlikely {
-                child.as_node().detach();
+            if !unlikely && UNLIKELY_CANDIDATE.binary_search(&&word[..]).is_ok() {
+                unlikely = true;
             }
         }
+
+        return unlikely;
+    }
+
+    fn convert_div_if_needed(&self, div: &ElemRef) {
+        let node = div.as_node();
+
+        if self.has_single_p(node) {
+            let p = node.children().elements().next().unwrap();
+            node.replace(p);
+        } else if !self.has_block_elem(node) {
+            node.rename(tag!("p"));
+        }
+    }
+
+    fn has_single_p(&self, node: &NodeRef) -> bool {
+        let mut child_it = node.children().elements();
+        let child = child_it.next();
+
+        if child.is_none() || child_it.next().is_some() {
+            return false;
+        }
+
+        let child = child.unwrap();
+        if !child.is(tag!("p")) {
+            return false;
+        }
+
+        let no_text = node.children().text_nodes().all(|t| t.borrow().trim().is_empty());
+        return no_text;
+    }
+
+    fn has_block_elem(&self, node: &NodeRef) -> bool {
+        node.descendants().elements().any(|elem| match elem.name {
+            tag!("a") | tag!("blockquote") | tag!("dl") | tag!("div") | tag!("img") | tag!("ol") |
+            tag!("p") | tag!("pre") | tag!("table") | tag!("ul") | tag!("select") => true,
+            _ => false
+        })
     }
 }
 
